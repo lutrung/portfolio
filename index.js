c5df0777f2607391b5664f2eefee2ec2a100a355
unknown <trunglu010297@gmail.com>
2022-06-12 11:13:15 +0700

    modal img

--- index.js
+++ index.js
@@ -34,3 +34,24 @@ let toAbout = () => {
     sidebar()
     toAbout()
 })()
+// MODAL
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+let certificate_img = document.querySelectorAll('.certificate_img')
+let btn_eye = document.querySelectorAll('.btn_eye')
+btn_eye.forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        modal.style.display = "block";
+          modalImg.src = certificate_img[index].src;
+          captionText.innerHTML = certificate_img[index].alt;
+    })
+})
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
