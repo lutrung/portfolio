ef2603cf216f7741d839129c6e40a91ec59e8f3e
Trung Lu Nguyen Thanh <trunglnt@digi-texx.local>
2022-06-15 16:04:00 +0700

    responsive

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let sidebar = () => {
     let menu = document.querySelectorAll('.menu_item')
+    let menuMB = document.querySelectorAll('.menu_itemMB')
     let main_item = document.querySelectorAll('.portfolio_item')
 
     let removeActive = (arr) => {
@@ -21,7 +22,17 @@ let sidebar = () => {
             }
         })
     })
+    menuMB.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            removeActive(main_item)
+            main_item[index].classList.add('active')
+            if (!item.classList.contains('active')) {
+                removeActive(menuMB)
+                item.classList.add('active')
 
+            }
+        })
+    })
 }
 let toAbout = () => {
     let menu = document.querySelectorAll('.menu_item')
@@ -30,9 +41,41 @@ let toAbout = () => {
         menu[1].click()
     })
 }
+let toHire = () => {
+    let menu = document.querySelectorAll('.menu_item')
+    let hireMe = document.getElementById('hireMe')
+    hireMe.addEventListener("click", () => {
+        menu[3].click()
+    })
+}
+let sendMail = () => {
+    let btnSend = document.getElementById('btnSendMe')
+    btnSend.addEventListener('click', (e) => {
+        let name = document.getElementById('name').value;
+        let email = document.getElementById('email').value;
+        let subject = document.getElementById('subject').value;
+        let message = document.getElementById('message').value;
+        let body = 'Name: ' + name + '<br/> Email:' + email + '<br/> Subject:' + subject + '<br/> Message:' + message;
+        e.preventDefault()
+        Email.send({
+            Host: "smtp.yourisp.com",
+            Username: "lutrung1297@gmail.com",
+            Password: "psqgunlncxummfoe",
+            To: 'lutrung1297@gmail.com',
+            From: email,
+            Subject: subject,
+            Body: body
+        }).then(
+            message => alert(message)
+        );
+    })
+
+}
 (start = () => {
     sidebar()
     toAbout()
+    toHire()
+    sendMail()
 })()
 // MODAL
 var modal = document.getElementById("myModal");
@@ -40,11 +83,11 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 let certificate_img = document.querySelectorAll('.certificate_img')
 let btn_eye = document.querySelectorAll('.btn_eye')
-btn_eye.forEach((item,index)=>{
-    item.addEventListener("click",()=>{
+btn_eye.forEach((item, index) => {
+    item.addEventListener("click", () => {
         modal.style.display = "block";
-          modalImg.src = certificate_img[index].src;
-          captionText.innerHTML = certificate_img[index].alt;
+        modalImg.src = certificate_img[index].src;
+        captionText.innerHTML = certificate_img[index].alt;
     })
 })
 
@@ -52,6 +95,6 @@ btn_eye.forEach((item,index)=>{
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
